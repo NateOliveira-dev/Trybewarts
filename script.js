cbca2b7c1862a38d5b13bf7c4b0343900da220ee
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-08-11 17:54:04 -0300

    adiciona js do requitiso 18

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const login = document.getElementById('input-login');
 const password = document.getElementById('input-senha');
-const submit = document.getElementById('btn-entrar');
+const enter = document.getElementById('btn-entrar');
 
-submit.addEventListener('click', () => {
+enter.addEventListener('click', () => {
   if (login.value === 'tryber@teste.com'
   && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+const submit = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
+
+agree.addEventListener('click', () => {
+  if (agree.checked) {
+    submit.disabled = false;
   }
 });
